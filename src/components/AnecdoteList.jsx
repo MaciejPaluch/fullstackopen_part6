b17a5584bf686e2c7867cac7e76294b4cd445a4b
Maciej Paluch <maciekpaluch02@gmail.com>
2026-03-12 22:15:24 +0100

    6.9

--- src/components/AnecdoteList.jsx
+++ src/components/AnecdoteList.jsx
@@ -3,7 +3,9 @@ import { voteForAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () =>{
     const anecdotes = useSelector(state => 
-  [...state].sort((a, b) => b.votes - a.votes)
+  [...state.anecdotes]
+  .filter(a => a.content.toLowerCase().includes(state.filter.toLowerCase()))
+  .sort((a, b) => b.votes - a.votes)
 )
   const dispatch = useDispatch()
 
